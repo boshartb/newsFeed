ed6f4e982d72bde9f24341a73208ba2eb9a48c2e
Brant <boshartb@gmail.com>
2018-11-26 12:54:16 -0500

    Data.js Info comp refactored and exported properly

--- src/component/App.js
+++ src/component/App.js
@@ -1,36 +1,7 @@
 import React, { Component } from "react";
-// import Newsfeed from "./Newsfeed";
+import Info from "./Info";
 import TopNav from "./TopNav";
 import newsFeedData from "./data";
-
-function Info(props) {
-  const content = props.newsFeedData.map(newsFeedDataObject => (
-    <div>
-      <ul class="list-group-item">
-        <ul class="list-unstyled">
-          <li class="media">
-            <img
-              class="avatar mr-3 rounded-0"
-              src={newsFeedDataObject.image}
-              alt="Generic placeholder image"
-            />
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">{newsFeedDataObject.title} </h5>
-              <p>{newsFeedDataObject.tags}</p>
-              <p>{newsFeedDataObject.impressions}</p>
-            </div>
-          </li>
-        </ul>
-      </ul>
-    </div>
-  ));
-
-  return (
-    <div>
-      <ul> {content}</ul>
-    </div>
-  );
-}
 
 class App extends Component {
   render() {
